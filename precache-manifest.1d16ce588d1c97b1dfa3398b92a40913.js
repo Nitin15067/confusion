f80611348d8faf0d5021225537f93402aef984bb
Nitin15067 <nitin15067@iiitd.ac.in>
2020-05-23 11:41:01 +0530

    update

---
+++ precache-manifest.1d16ce588d1c97b1dfa3398b92a40913.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02c207457dd60ca45973d3c3f3c9ebb",
+    "url": "/confusion/index.html"
+  },
+  {
+    "revision": "b262069239a3e51d4a71",
+    "url": "/confusion/static/css/2.e1948333.chunk.css"
+  },
+  {
+    "revision": "7b52eeeeccd8ff9289bf",
+    "url": "/confusion/static/css/main.45435c08.chunk.css"
+  },
+  {
+    "revision": "b262069239a3e51d4a71",
+    "url": "/confusion/static/js/2.57d0135d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/confusion/static/js/2.57d0135d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b52eeeeccd8ff9289bf",
+    "url": "/confusion/static/js/main.2cda4a47.chunk.js"
+  },
+  {
+    "revision": "93e338425105545936e2",
+    "url": "/confusion/static/js/runtime-main.dfde7455.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
